d74d9fe9f1b383b23a6a5669afba40fe2e265b54
Aaron Case <aacase@gmail.com>
2014-07-17 17:42:06 -0500

    Created Rspec for wishes str

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -8,7 +8,18 @@ describe 'Exercise 0' do
     result = Exercises.ex0("ha")
     expect(result).to eq("hahaha")
   end
+
+  it "returns 'nope' if string = wishes" do
+    result = Exercises.ex0("wishes")
+    expect(result).to eq('nope')
+  end
 end
+
+
+#   describe "Exercise 1" do
+#     xit "returns the number of elements in array"
+#   end
+# end
 
 
 
